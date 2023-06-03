516bb468e6e450a542b9ce7e0439a31e4aa56698
jeyem <me@e-mahmoudi.me>
2023-06-03 19:50:32 +0330

    lending and withdrawn models

--- src/apps/projects/models.py
+++ src/apps/projects/models.py
@@ -4,6 +4,16 @@ from src.apps.users.models import User
 
 
 class Project(models.Model):
+    STATUS_COMPLETE = 'COMPLETE'
+    STATUS_FUNDRAISING = 'FUNDRAISING'
+    STATUS_EXPIRED = 'EXPIRED'
+
+    STATUS_CHOICES = (
+        (STATUS_COMPLETE, 'Compelete'),
+        (STATUS_FUNDRAISING, 'Fundraising'),
+        (STATUS_EXPIRED, 'expired')
+    )
+
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=120)
@@ -12,6 +22,54 @@ class Project(models.Model):
     location = models.CharField(max_length=5, null=True)
     loan_amount = models.IntegerField()
     repayment_period = models.IntegerField()
+    status = genre = models.CharField(
+        max_length=16, choices=STATUS_CHOICES, default=STATUS_FUNDRAISING)
 
     class Meta:
         db_table = 'projects'
+
+
+class Lend(models.Model):
+    STATUS_PENDING = 'PENDING'
+    STATUS_LENDED = 'LENDED'
+    STATUS_REFUNDED = 'REFUNDED'
+
+    STATUS_CHOICES = (
+        (STATUS_PENDING, 'Pending'),
+        (STATUS_LENDED, 'Lended'),
+        (STATUS_REFUNDED, 'Refunded')
+    )
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL)
+    project = models.ForeignKey(Project, on_delete=models.SET_NULL)
+    amount = models.IntegerField()
+    transaction_id = models.CharField(max_length=300)
+    refund_transaction_id = models.CharField(max_length=300)
+
+    status = genre = models.CharField(
+        max_length=16, choices=STATUS_CHOICES, default=STATUS_PENDING)
+
+    def save(self, *args, **kwargs):
+        if self.refund_transaction_id:
+            self.status = self.STATUS_REFUNDED
+        super().save(*args, **kwargs)
+
+    class Meta:
+        db_table = 'lends'
+
+
+class Withdrawn(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL)
+    project = models.ForeignKey(Project, on_delete=models.SET_NULL)
+    total_amount = models.IntegerField()
+    transaction_id = models.CharField(max_length=300)
+
+    def save(self, *args, **kwargs):
+        Lend.objects.filter(project=self.project).update(
+            status=Lend.STATUS_LENDED)
+        super().save(*args, **kwargs)
+
+    class Meta:
+        db_table = 'withrawns'

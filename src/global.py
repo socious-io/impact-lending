e37a69509cee4e2b05e415979963e38b3979b6ea
jeyem <me@e-mahmoudi.me>
2023-06-20 05:21:04 +0330

    remove bundle catch

--- src/global.py
+++ src/global.py
@@ -1,5 +1,10 @@
 from django.conf import settings
+from time import time
 
 
 def vars(request):
-    return {'dapp_env': settings.DAPP_ENV}
+    return {
+        'dapp_env': settings.DAPP_ENV,
+        'time': int(time())
+
+    }
